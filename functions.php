4bc56bb5f2e128a368ee5c931d2fba867f65fdac
mirkoschubert <office@mirkoschubert.de>
2020-12-14 14:30:29 +0100

    switch to live version

--- functions.php
+++ functions.php
@@ -59,11 +59,11 @@ add_filter('auto_core_update_send_mail', 'divi_child_stop_update_mails', 10, 4);
 
 
 /**
- * Adds SVG support for file uploads
+ * Adds SVG & WebP support for file uploads
  */
 function divi_child_supported_filetypes($filetypes) {
 
-  $new = array('svg' => 'image/svg+xml', 'svg' => 'image/svg');
+  $new = array('svg' => 'image/svg+xml', 'svg' => 'image/svg', 'webp' => 'image/webp');
   return array_merge($filetypes, $new);
 }
 add_action('upload_mimes', 'divi_child_supported_filetypes');
